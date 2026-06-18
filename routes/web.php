ca77ed03c934152520028c7418c95da920146802
gilcris1980 <gilcrischatto@gmail.com>
2026-06-18 15:23:27 +0800

    Add /mail-test route for debugging mail config

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,15 @@ Route::get('/', function () {
         'message' => 'ChattoShop API is running'
     ]);
 });
+
+Route::get('/mail-test', function () {
+    return [
+        'mailer' => config('mail.default'),
+        'host' => config('mail.mailers.smtp.host'),
+        'port' => config('mail.mailers.smtp.port'),
+        'encryption' => config('mail.mailers.smtp.encryption'),
+        'username' => config('mail.mailers.smtp.username'),
+        'from' => config('mail.from.address'),
+        'from_name' => config('mail.from.name'),
+    ];
+});
